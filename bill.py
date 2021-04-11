a5cb6a71e1626415c82d078fe8dab22f4e69c823
Bismarck6502 <sam@aepearce.com>
2021-04-11 16:40:13 +0200

    finished File Generation

--- bill.py
+++ bill.py
@@ -2,16 +2,105 @@
 # Bill Class for Bill Script
 #
 
+from io import StringIO
+import csv
+import datetime
+
+
+class BillInfo:
+    def __init__(self, csv_line):
+        field_names = [
+            'bill_num', 'job_num', 'sender_location',
+            'bill_date', 'bill_time', 'payment_target'
+        ]
+        f = StringIO(csv_line)
+        reader = csv.DictReader(f, delimiter=';', fieldnames=field_names)
+        for row in reader:
+            self.bill_num = row['bill_num'].removeprefix('Rechnung_')  # Get rid of 'Rechnung' label
+            self.job_num = row['job_num'].removeprefix('Auftrag_')  # Get rid of 'Auftrag' label
+            self.sender_location = row['sender_location']
+            self.bill_date = datetime.datetime.strptime(row['bill_date'], '%d.%m.%Y')
+            self.bill_time = row['bill_time']
+            self.payment_target = row['payment_target'].removeprefix('ZahlungszielInTagen_')  # Get rid of label
+
+
 class Sender:
-	def __init__(self, csv_line):
-		self.csv_line = csv_line
-		
-		
+    def __init__(self, csv_line):
+        field_names = [
+            'label', 'party_ID', 'customer_num', 'name',
+            'address', 'postcode_and_city', 'company_ID', 'email'
+        ]
+        f = StringIO(csv_line)
+        reader = csv.DictReader(f, delimiter=';', fieldnames=field_names)
+        for row in reader:
+            self.party_ID = row['party_ID']
+            self.customer_num = row['customer_num']
+            self.name = row['name']
+            self.address = row['address']
+            self.postcode_and_city = row['postcode_and_city']
+            self.company_ID = row['company_ID']
+            self.email = row['email']
+
+
+class Receiver:
+    def __init__(self, csv_line):
+        field_names = [
+            'label', 'customer_ID', 'name',
+            'address', 'postcode_and_city'
+        ]
+        f = StringIO(csv_line)
+        reader = csv.DictReader(f, delimiter=';', fieldnames=field_names)
+        for row in reader:
+            self.customer_ID = row['customer_ID']
+            self.name = row['name']
+            self.address = row['address']
+            self.postcode_and_city = row['postcode_and_city']
+
+
+class BillItem:
+    def __init__(self, csv_line):
+        field_names = [
+            'label', 'item_num', 'item_label',
+            'amount', 'unit_price', 'total', 'VAT'
+        ]
+        f = StringIO(csv_line)
+        reader = csv.DictReader(f, delimiter=';', fieldnames=field_names)
+        for row in reader:
+            self.item_num = row['item_num']
+            self.item_label = row['item_label']
+            self.amount = row['amount']
+            self.unit_price = row['unit_price']
+            self.total = row['total']
+            self.VAT = row['VAT'].removeprefix('MWST_')  # Get rid of 'MWST' label
+
 
 class Bill:
-	def __init__(self, filename):
-		self.filename = filename
-		
-		with open(filename, 'r') as f:
-			lines = f.readlines()
-		
+    def __init__(self, filename):
+        self.filename = filename
+        self.items = []
+
+        with open(filename, 'r') as f:
+            for line in f:
+                # TODO: Constants/Config?
+                if line.startswith('Rechnung'):
+                    self.billInfo = BillInfo(line)
+                elif line.startswith('Herkunft'):
+                    self.sender = Sender(line)
+                elif line.startswith('Endkunde'):
+                    self.receiver = Receiver(line)
+                elif line.startswith('RechnPos'):
+                    self.items.append(BillItem(line))  # Add to list of items
+
+        # Calculate other required info
+        self.billInfo.totalVAT = 0.0
+        for item in self.items:
+            self.billInfo.totalVAT += (float(item.VAT.removesuffix('%')) / 100) * float(item.total)
+
+        self.billInfo.total = self.billInfo.totalVAT
+        for item in self.items:
+            self.billInfo.total += float(item.total)
+
+        self.billInfo.total_fr = int(self.billInfo.total)
+        self.billInfo.total_rp = int(self.billInfo.total - int(self.billInfo.total))
+
+        self.billInfo.pay_by = self.billInfo.bill_date + datetime.timedelta(days=int(self.billInfo.payment_target))
--- getBills.py
+++ getBills.py
@@ -4,17 +4,17 @@
 # versions on to the payment server.
 #
 # Samuel Pearce
-
+from datetime import datetime
+import chevron
 from ftplib import FTP
-
-import time
-import csv
-
-import bill.py
+from bill import Bill
 
 # Constants
 LOG_FILE = './latest.log'
+TXT_TEMPLATE_FILENAME = 'txt_template.txt'
+XML_TEMPLATE_FILENAME = 'xml_template.txt'
 
+# TODO: Config File
 ABHOLSERVER_HOSTNAME = 'ftp.haraldmueller.ch'
 ABHOLSERVER_USERNAME = 'schoolerinvoices'
 ABHOLSERVER_PASSWORD = 'Berufsschule8005!'
@@ -24,44 +24,130 @@ PAYSERVER_HOSTNAME = '134.119.225.245'
 PAYSERVER_USERNAME = '310721-297-zahlsystem'
 PAYSERVER_PASSWORD = 'Berufsschule8005!'
 
+
+# Functions
+def log(str):
+    now = datetime.now()
+    current_time = now.strftime("[%H:%M:%S] ")
+    logfile.write(current_time + str + '\n')
+
+
 logfile = open(LOG_FILE, 'w')
-nowstr = time.asctime(time.localtime())
-logfile.write(nowstr + '\n -- LATEST LOG --\n\n')
+logfile.write(' -- Bill Retriever Log --\n\n')
 
 # Connect to Server
-logfile.write('Connecting to server...\n')
+log('Connecting to server...')
 ftp = FTP(ABHOLSERVER_HOSTNAME)
 ftp.login(ABHOLSERVER_USERNAME, ABHOLSERVER_PASSWORD)
 # TODO: Check if connection was successful
-logfile.write('Logged in successfully.\n')
+log('Logged in successfully.')
 ftp.cwd(ABHOLSERVER_PATH)
 
 # Get names of new bills
-bills = []
+bill_names = []
+
+
 def getBillNames(name):
-	if name.endswith('.data'):
-		bills.append(name)
+    if name.endswith('.data'):
+        bill_names.append(name)
+
+
 ftp.retrlines('NLST', getBillNames)
-logfile.write('Retrieved bill names.\n')
+log('Retrieved bill names.')
 
 # Get every bill file and delete it from the server
-for bill in bills:
-	with open(bill, 'wb') as bd:
-		ftp.retrbinary('RETR ' + bill, bd.write)
-	ftp.delete(bill)
-logfile.write('Retrieved and deleted bill files.\nClosing connection.\n')
+for bill in bill_names:
+    with open(bill, 'wb') as bd:
+        ftp.retrbinary('RETR ' + bill, bd.write)
+    # DEBUG: Don't delete file from server # ftp.delete(bill)
+log('Retrieved and deleted bill files.')
+log('Closing connection.')
 ftp.quit()
 
 # Parse Datafile
-logfile.write('Parsing Data...\n')
-fieldNames = [
-	'rechnungsnummer', 'auftragsnummer', 'absendeort', 'rechnungsdatum', 'rechnungszeit', 'zahlungsziel',
-	'herkunft', 'party_ID', 'kundennummer', 'name', 'adresse', 'plz_und_ort', '
-	]
-for bill in bills:
-	with open(bill, 'r') as bd:
-		reader = csv.DictReader(bill, delimiter=';')
-		for row in reader:
-			
+log('Parsing Data...')
+bills = []
+for bill_file in bill_names:
+    bills.append(Bill(bill_file))
+# TODO: Check parsing was successful
+log('Data Parsed Successfully.')
+
+for curr_bill in bills:
+    # Generate Text Output
+    log('Generating human-readable file...')
+    with open(TXT_TEMPLATE_FILENAME, 'r') as template:
+
+        fields = {
+            'sender_name': 			curr_bill.sender.name,
+            'sender_address': 		curr_bill.sender.address,
+            'sender_postcode': 		curr_bill.sender.postcode_and_city,
+            'sender_company_name': 	curr_bill.sender.company_ID,
+            'sender_location': 		curr_bill.billInfo.sender_location,
+            'bill_date': 			curr_bill.billInfo.bill_date.strftime('%d.%m.%Y'),
+            'receiver_name': 		curr_bill.receiver.name,
+            'receiver_address': 	curr_bill.receiver.address,
+            'receiver_postcode': 	curr_bill.receiver.postcode_and_city,
+            'customer_number': 		curr_bill.receiver.customer_ID,
+            'job_number': 			curr_bill.billInfo.job_num,
+            'bill_number': 			curr_bill.billInfo.bill_num,
+            'items': [],
+            'bill_total_space':     ' ' * (16 - len(str(curr_bill.billInfo.total))),
+            'bill_total': 		    curr_bill.billInfo.total,
+            'VAT_total_space':      ' ' * (16 - len(str(curr_bill.billInfo.totalVAT))),
+            'VAT_total': 		    curr_bill.billInfo.totalVAT,
+            'payment_target': 	    curr_bill.billInfo.payment_target,
+            'pay_by_date': 		    curr_bill.billInfo.pay_by.strftime('%d.%m.%Y'),
+            'bill_total_fr': 	    curr_bill.billInfo.total_fr,
+            'bill_total_rp': 	    curr_bill.billInfo.total_rp,
+        }
+        for curr_item in curr_bill.items:
+            fields.get('items').append({
+                'item_number': curr_item.item_num,
+                'item_label': curr_item.item_label,
+                'item_amount_space': ' ' * (40 - len(curr_item.item_label)),
+                'amount': curr_item.amount,
+                'unit_price_space': ' ' * (11 - len(curr_item.unit_price)),
+                'unit_price': curr_item.unit_price,
+                'total_space': ' ' * (12 - len(str(curr_item.total))),
+                'total': curr_item.total,
+                'VAT': curr_item.VAT
+            })
+
+        rendered_txt = chevron.render(template, fields)
+        log('TXT File Generated Successfully.')
+
+    # Generate XML Output
+    log('Generating XML file...')
+    with open(XML_TEMPLATE_FILENAME, 'r') as template:
+
+        fields = {
+            'party_ID': curr_bill.sender.party_ID,
+            'ref_num': datetime.now().strftime('%Y%m%d%H%M%S'),
+            'date_squash': curr_bill.billInfo.bill_date.strftime('%Y%m%d'),
+            'job_num': curr_bill.billInfo.job_num,
+            'company_ID': curr_bill.sender.company_ID,
+            'sender_name': curr_bill.sender.name,
+            'sender_address': curr_bill.sender.address,
+            'sender_postcode': curr_bill.sender.postcode_and_city,
+            'receiver_name': curr_bill.receiver.name,
+            'receiver_address': curr_bill.receiver.address,
+            'receiver_postcode': curr_bill.receiver.postcode_and_city,
+            'total_squash': str(int(curr_bill.billInfo.total * 10)).zfill(10),
+            'bill_date_day': curr_bill.billInfo.bill_date.day,
+            'payment_target': curr_bill.billInfo.payment_target,
+            'pay_by_squash': curr_bill.billInfo.pay_by.strftime('%Y%m%d'),
+        }
+
+        rendered_xml = chevron.render(template, fields)
+        log('XML File Generated Successfully.')
+
+    # Write output Files
+    out_filename = curr_bill.sender.customer_num + '_' + curr_bill.billInfo.bill_num + '_invoice'
+    with open(out_filename + '.txt', 'w') as txt_file:
+        txt_file.write(rendered_txt)
+    with open(out_filename + '.xml', 'w') as xml_file:
+        xml_file.write(rendered_xml)
+
+    log('Generated text file.')
 
 logfile.close()
